5355451bc07e2a605ab52677a0b5f53b4b5d99e1
Hieu Man <hieuga2708@gmail.com>
2023-07-07 04:12:41 +0000

    init

---
+++ diffus_ie/data_modules/constants.py
@@ -0,0 +1,6 @@
+ESL_LABEL = {'FALLING_ACTION': [0, "'{e1}' is caused by '{e2}'"], 
+            'PRECONDITION': [1, "'{e1}' causes '{e2}'"], 
+            'NoRel': [2, "'{e1}' has no relation to '{e2}'"]}
+
+CTB_LABEL = {'PRECONDITION': [0, "'{e1}' causes '{e2}'"], 
+            'NoRel': [1, "'{e1}' has no relation to '{e2}'"]}
